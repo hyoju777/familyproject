2caffa24e1048eac7b819109e9d10e8001f4d942
hyoju <claudius7777@naver.com>
2024-09-11 16:42:19 +0900

    240911

---
+++ familystory/src/test/java/com/ohgiraffers/familystory/FamilystoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.familystory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilystoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
